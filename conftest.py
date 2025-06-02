6ab486a341e4a7c57ded1c9bdc4cf4b83b4a2e8c
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-06-02 17:52:49 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,14 @@
 import pytest
 from selenium import webdriver
+
+# Using fixture to preapre the test environment (to open the browser and use it)
+# reusable purpose
+
 @pytest.fixture
 def driver_method():
+
     driver=webdriver.Chrome()
+    
     driver.get("https://www.saucedemo.com/")
+    
     return driver
