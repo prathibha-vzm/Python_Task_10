07f54c15188af831d374845165897628c5cd994d
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-06-02 17:22:45 +0530

    test_home_url.py

--- test_home_url.py
+++ test_home_url.py
@@ -3,6 +3,12 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
+
+#Positive-test case -> Fetch the home page URL
+#Using currenturl to fetch the URL and checking it using assert
+#fetching URL before log in 
+#marking for sanity test
+
 @pytest.mark.sanity
 def test_valid_current_login_url(driver_method):
 
@@ -21,6 +27,11 @@ def test_valid_current_login_url(driver_method):
     print("URL of this Page is ", url)
 
     assert url == 'https://www.saucedemo.com/'
+
+
+#Negative-test case -> Fetching Invalid URL
+#Using currenturl to fetch the URL and checking it using assert
+#marking for regression test
 
 @pytest.mark.regression
 def test_invalid_current_url_homepage(driver_method):
