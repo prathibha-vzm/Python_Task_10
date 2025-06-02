60f671343981f548fde6f6c6dee86b94789a1a48
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-06-02 17:57:34 +0530

    test_write_web_page.py

--- test_write_web_page.py
+++ test_write_web_page.py
@@ -3,13 +3,6 @@ from selenium.webdriver.common.by import By
 
 from venv_web_page.Util.login_utils import LoginUtils
 
-# Question: 3 Fetch all the content from the webpage, Fetch Title, Fetch Current URL
-# Log in the page using verify_user_login method
-# Fetching the content using TAGNAME locator and text webelement
-# In with block Creating a Webpage_task_11.txt using open method
-# and setting it to write mode
-# writing the text to the file
-#marked as smoke - this check the flow from home to dashboard page.
 
 @pytest.mark.smoke
 def test_extracting_text_from_webpage(driver_method):
@@ -27,3 +20,11 @@ def test_extracting_text_from_webpage(driver_method):
 
     with open("Webpage_task_11.txt",'w') as file:
         file.write(text)
+
+# Question: 3 Fetch all the content from the webpage, Fetch Title, Fetch Current URL
+# Log in the page using verify_user_login method
+# Fetching the content using TAGNAME locator and text webelement
+# In with block Creating a Webpage_task_11.txt using open method
+# and setting it to write mode
+# writing the text to the file
+#marked as smoke - this check the flow from home to dashboard page.
