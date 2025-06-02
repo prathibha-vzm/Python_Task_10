ede3f523113a962ff02c9e6c1b1e375a633e279f
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-06-02 17:46:13 +0530

    Update test_write_web_page.py

--- test_write_web_page.py
+++ test_write_web_page.py
@@ -3,12 +3,19 @@ from selenium.webdriver.common.by import By
 
 from venv_web_page.Util.login_utils import LoginUtils
 
+# Question: 3 Fetch all the content from the webpage, Fetch Title, Fetch Current URL
+# Log in the page using verify_user_login method
+# Fetching the content using TAGNAME locator and text webelement
+# In with block Creating a Webpage_task_11.txt using open method
+# and setting it to write mode
+# writing the text to the file
 
 @pytest.mark.smoke
 def test_extracting_text_from_webpage(driver_method):
 
     util = LoginUtils()
     util.verify_user_login(driver_method)
+
     driver = driver_method
 
     print(driver.title)
