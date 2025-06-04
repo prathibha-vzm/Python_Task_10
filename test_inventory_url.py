82d6b0c798327e7303cfb9747bbb33d87546acc4
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-06-04 10:56:04 +0530

    test_inventory_url.py

--- test_inventory_url.py
+++ test_inventory_url.py
@@ -1,13 +1,12 @@
 #Importing Packages
-
+#Importing login.utils to access the method in the class
 import pytest
 from venv_web_page.Util.login_utils import LoginUtils
 
 #positive-test case -> Login and fetch the dashboard URL
-#importing login.utils 
-#creating Object for class and accessing the method to log in the page
-#driver_method is a function used to open the web page and use the URL
-#using assert to check the expected with actual
+# 14: creating Object for class and (15:) accessing the method to log in the page
+# 17: driver_method is a function used to open the web page and use the URL - conftest.py
+# 20: using assert to check the expected with actual
 
 @pytest.mark.sanity
 def test_valid_current_inventory_url(driver_method):
@@ -22,7 +21,7 @@ def test_valid_current_inventory_url(driver_method):
 
 
 #Negative-test case -> fetching unexpected dashboard URL
-#marked as a regression
+# 26: marked as a regression
 
 @pytest.mark.regression
 def test_invalid_current_url_inventory_page(driver_method):
