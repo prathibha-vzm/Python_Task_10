70938336503528ce95848c975e6f7afae8248d20
prathibha-vzm <vigneswaranprathibha@gmail.com>
2025-06-04 11:01:40 +0530

    test_write_web_page.py

--- test_write_web_page.py
+++ test_write_web_page.py
@@ -1,4 +1,5 @@
-#importing packages and class
+#importing packages
+#Importing LoginUtils to access the method
 
 import pytest
 from selenium.webdriver.common.by import By
@@ -23,9 +24,9 @@ def test_extracting_text_from_webpage(driver_method):
         file.write(text)
 
 # Question: 3 Fetch all the content from the webpage, Fetch Title, Fetch Current URL
-# Log in the page using verify_user_login method
-# Fetching the content using TAGNAME locator and text webelement
-# In with block Creating a Webpage_task_11.txt using open method
-# and setting it to write mode
-# writing the text to the file
-#marked as smoke - this check the flow from home to dashboard page.
+# 12 & 13: Log in the page using verify_user_login method
+# 21: Fetching the content using TAGNAME locator and text webelement
+# 23: In with block Creating a Webpage_task_11.txt using open method
+# 23: and setting it to write mode
+# 24: writing the text to the file
+# 9: marked as smoke - this check the flow from home to dashboard page.
